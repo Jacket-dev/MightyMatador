6f5e2d42d114aaff3a1a1e0708b58993574ff805
Jacket <melen.louis@gmail.com>
2022-11-10 16:52:32 +0100

    Added Tags to wall and ground + raycast tentative

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private CapsuleCollider2D capsuleCollider;
     public float moveSpeed;
     public float jumpSpeed;
     private bool grounded;
@@ -13,16 +14,20 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        grounded = true;
         rb.velocity = new Vector2(Input.GetAxis("Horizontal")*moveSpeed,0);
-        if(Input.GetAxis("Jump")!=0 && grounded)
+        grounded = Physics2D.Raycast(transform.position, -Vector2.up, capsuleCollider.bounds.extents.y+.1f);
+        Debug.Log("grounded " + grounded);
+        if(grounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, Input.GetAxis("Jump")*jumpSpeed);
+            grounded=false;
         }
+
     }
 }
