32f74cce1b781abea00b84605b8fd2d6c0f9f590
Jacket <melen.louis@gmail.com>
2022-11-10 15:10:36 +0100

    Pushed Scene this time :sweat_smile:

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
     public float moveSpeed;
+    public float jumpSpeed;
+    private bool grounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        grounded = true;
         rb.velocity = new Vector2(Input.GetAxis("Horizontal")*moveSpeed,0);
+        if(Input.GetAxis("Jump")!=0 && grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Input.GetAxis("Jump")*jumpSpeed);
+        }
     }
 }
