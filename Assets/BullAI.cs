3698a14def319b473c88fae0798d23d407f62205
Jacket <melen.louis@gmail.com>
2022-11-16 15:50:16 +0100

    modified bullAI + tried changing player jump code
    Jump code isn't working rn, player can jump multiple times in the air if we put jumpTime!=0. Needs fix

--- Assets/BullAI.cs
+++ Assets/BullAI.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class BullAI : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+    private bool facingLeft;
+    public float bullSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        facingLeft = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(facingLeft)
+        {
+            rb.velocity = Vector2.left * bullSpeed;
+        }
+        else rb.velocity = Vector2.right * bullSpeed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            facingLeft = !facingLeft;
+            transform.localScale = new Vector2(-transform.localScale.x,transform.localScale.y);
+
+        }
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("PlayerIsDead");
+        }
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,9 +11,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private CapsuleCollider2D capsuleCollider;
     private float jumpTime;
+    private bool grounded;
+    private bool jumping;
     public float moveSpeed;
     public float jumpSpeed;
-    private bool grounded;
     public float maxJumpTime;
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
         jumpTime = 0;
+        jumping= false;
     }
 
     // Update is called once per frame
@@ -26,19 +30,31 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal")*moveSpeed, rb.velocity.y);
         RaycastHit2D groundedRayCast = Physics2D.Raycast(capsuleCollider.bounds.center, Vector2.down, capsuleCollider.bounds.extents.y+.1f, LayerMask.GetMask("Ground"));
-        IsGrounded();
-        jumpTime=Jump(jumpTime,maxJumpTime);
-        Debug.Log(jumpTime);
+        //jumpTime=Jump(jumpTime,maxJumpTime,jumping);
         /*if(IsGrounded())
         {
             rb.velocity.Set(rb.velocity.x, Input.GetAxis("Jump")*jumpSpeed);
         }*/
 
+        if (Input.GetAxis("Jump") != 0 && IsGrounded())
+        {
+            jumping = true;
+            jumpTime = 0;
+        }
+        if(jumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Input.GetAxis("Jump") * jumpSpeed);
+            jumpTime += Time.deltaTime;
+        }
+        if(jumpTime>maxJumpTime || Input.GetAxis("Jump")==0)
+        {
+            jumping = false;
+        }
 
     }
 
 
-    private float Jump(float jumpTime,float maxJumpTime)
+    /*private float Jump(float jumpTime,float maxJumpTime) //TODO change this so it works
     {
         if(Input.GetAxis("Jump")!=0)
         {
@@ -50,16 +66,20 @@ public class PlayerController : MonoBehaviour
             if(jumpTime<maxJumpTime)
             {
                 jumpTime+=Time.deltaTime;
-                rb.velocity = new Vector2(rb.velocity.x, Input.GetAxis("Jump") * jumpSpeed);
+                rb.velocity = new Vector2(rb.velocity.x, Input.GetAxis("Jump") * jumpSpeed); //Can jump again after launching from ground, needs changes
             }
         }
         return jumpTime;
+    }*/
+    private float Jump(float jumpTime, float maxJumpTime, bool jumping) //TODO change this so it works
+    {
+        return jumpTime;
     }
+
     private bool IsGrounded()
     {
         Color rayColor;
         RaycastHit2D groundedRayCast = Physics2D.Raycast(capsuleCollider.bounds.center, Vector2.down, capsuleCollider.bounds.extents.y + .1f, LayerMask.GetMask("Ground"));
-        Debug.Log(groundedRayCast.collider);
         if(groundedRayCast.collider!=null)
         {
             rayColor = Color.green;
